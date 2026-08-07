20f0c1b195b5ee063171d0c23e9cf16f9c2d00f8
honggyeongwoo <gyeongwoo97@gmail.com>
2026-08-07 17:21:29 +0900

    기본 세팅

---
+++ ediay-clone-team2/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* 은호 */}
+      <div>
+
+        
+      {/* 경우 */}
+      <div>
+
+        
+        
+      {/* 현호 */}
+      <div>
+
+        
+      </div>
+    </>
+  )
+}
+
+export default App
