f2106d491582405392fdbaa65b9d458d208cb33c
honggyeongwoo <gyeongwoo97@gmail.com>
2026-08-21 17:35:50 +0900

    2026/08/21 header

--- ediay-clone-team2/src/header/Ediya_header.jsx
+++ ediay-clone-team2/src/header/Ediya_header.jsx
@@ -1,117 +1,91 @@
+// Ediya_header.jsx
+
 import React from "react";
 
 import "./Ediya_Header_nav-bar.css";
 import "./Ediya_Header_menu-banner.css";
 import "./Ediya_Header_menu-header.css";
 
-// import "./Ediya_Header_language.js";
+// 🟩 [추가] 일반 JS 실행 파일이 아니라 React custom hook으로 연결
+import useEdiyaHeader from "./Ediya_Header_language.js";
 
 function EdiyaHeader() {
+  // 🟩 [추가] JS 파일에서 state와 click 함수를 받아 사용
+  const {
+    isLanguageOpen,
+    activeNav,
+    isMobileMenuOpen,
+    handleLanguageClick,
+    handleNavClick,
+    handleMobileMenuClick,
+    handleHeaderClick,
+  } = useEdiyaHeader();
+
+  const topNavItems = ["브랜드 사이트", "기업 사이트", "창업안내", "채용안내"];
+
   return (
-    // <!-- Header 전체 구역 -->
-    <header className="wrap-header">
-      {/* <!-- 1구역: 상단 Nav Bar (파란 배경) --> */}
+    // 🟩 [추가] 모든 # 링크의 기본 이동을 header 한 곳에서 차단
+    <header className="wrap-header" onClick={handleHeaderClick}>
+      {/* 1구역: 상단 Nav Bar */}
       <div className="nav-bar">
-        {/* <!-- 1. 왼쪽: 브랜드/기업/창업/채용 --> */}
-        <div className="nav-bar-left">
-          {/* <!-- ul-list 형식으로 배치 후 내부 Text는 A 태그로 링크 주기 --> */}
-          {/* <!-- 누른 사이트 페이지가 선택되어 있을때는 흰색 배경 색으로 변경되어 시각효과 적용 Class 붙임/때기 --> */}
+        <nav className="nav-bar-left" aria-label="상단 사이트 메뉴">
           <ul>
-            <li className="on">
-              <a href="">브랜드 사이트</a>
-            </li>
-            <li>
-              <a href="">기업 사이트</a>
-            </li>
-            <li>
-              <a href="">
-                창업안내
-              </a>
-            </li>
-            <li>
-              <a href="">
-                채용안내
-              </a>
-            </li>
+            {topNavItems.map((item, index) => (
+              <li key={item} className={activeNav === index ? "on" : ""}>
+                {/* 🟨 [수정] 모든 href를 #으로 통일 */}
+                <a href="#" onClick={(event) => handleNavClick(event, index)}>
+                  {item}
+                </a>
+              </li>
+            ))}
           </ul>
-        </div>
+        </nav>
 
-        {/* <!-- 오른쪽: 로그인 등 유틸 메뉴/SNS/검색 --> */}
         <div className="nav-bar-right">
-          {/* <!-- 로그인 등 유틸 메뉴 --> */}
           <ul className="nav-util">
-            <li>
-              <a href="https://members.ediya.com/member/login?referer_url=https://www.ediya.com/api/externalLoginRequest.php">
-                로그인
-              </a>
-            </li>
-            <li>
-              <a href="https://www.ediya.com/contents/find_store.html">
-                매장찾기
-              </a>
-            </li>
-            <li>
-              <a href="https://members.ediya.com/support/faq">
-                고객의 소리
-              </a>
-            </li>
-            <li>
-              <a href="https://ediyastore.com/index.html">
-                이디야 공식몰
-              </a>
-            </li>
-            <li>
-              <a href="https://brand.naver.com/ediyacoffeegift">
-                모바일상품권 스토어
-              </a>
-            </li>
+            <li><a href="#">로그인</a></li>
+            <li><a href="#">매장찾기</a></li>
+            <li><a href="#">고객의 소리</a></li>
+            <li><a href="#">이디야 공식몰</a></li>
+            <li><a href="#">모바일상품권 스토어</a></li>
           </ul>
 
-          {/* <!-- SNS 아이콘 영역 --> */}
           <ul className="nav-sns">
-            <li>
-              <a href="#">
-                <img src="./m_top_sns01.gif" alt="" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="./m_top_sns02.gif" alt="" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="./m_top_sns03.gif" alt="" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="./m_top_sns04.gif" alt="" />
-              </a>
-            </li>
+            <li><a href="#"><img src="./m_top_sns01.gif" alt="블로그" /></a></li>
+            <li><a href="#"><img src="./m_top_sns02.gif" alt="인스타그램" /></a></li>
+            <li><a href="#"><img src="./m_top_sns03.gif" alt="페이스북" /></a></li>
+            <li><a href="#"><img src="./m_top_sns04.gif" alt="유튜브" /></a></li>
           </ul>
 
-          {/* <!-- 검색 --> */}
-          <div className="nav-search">
-            <input type="text" placeholder="" />
-
-            <button>
-              <a href="">
-                <img src="./icon_search.gif" alt="" />
-              </a>
+          <form className="nav-search" onSubmit={(event) => event.preventDefault()}>
+            <label htmlFor="header-search" className="sr-only">검색</label>
+            <input id="header-search" type="text" />
+            {/* 🟨 [수정] button 안의 a 제거: 중첩 클릭 요소 오류 방지 */}
+            <button type="submit" aria-label="검색">
+              <img src="./icon_search.gif" alt="" />
             </button>
-          </div>
+          </form>
         </div>
       </div>
 
-      {/* <!-- //Nav Bar --> */}
-
-      {/* <!-- 2구역: 로고 + 언어 선택 --> */}
+      {/* 2구역: 로고 + 언어 선택 */}
       <div className="menu-banner">
-        <div className="banner-left"></div>
+        <div className="banner-left">
+          {/* 🟩 [추가] 768px 이하에서 표시되는 모바일 메뉴 버튼 */}
+          <button
+            type="button"
+            className="mobile-menu-button"
+            onClick={handleMobileMenuClick}
+            aria-label="전체 메뉴 열기"
+            aria-expanded={isMobileMenuOpen}
+          >
+            {isMobileMenuOpen ? "×" : "☰"}
+          </button>
+        </div>
 
         <div className="banner-logo">
-          <a href="https://ediya.com/">
+          {/* 🟨 [수정] 실제 이동을 막기 위해 href를 #으로 변경 */}
+          <a href="#">
             <img
               src="https://ediya.com/images/common/top_logo_240822.gif"
               alt="EDIYA COFFEE"
@@ -119,178 +93,100 @@ function EdiyaHeader() {
           </a>
         </div>
 
-        <div className="language-box">
-          <a href="" className="language-button">
-            <img
-              src="https://www.ediya.com/images/common/lang_ko.webp"
-              alt=""
-            />
+        {/* 🟨 [수정] React state에 따라 open 클래스 적용 */}
+        <div className={`language-box ${isLanguageOpen ? "open" : ""}`}>
+          <a
+            href="#"
+            className="language-button"
+            onClick={handleLanguageClick}
+            aria-expanded={isLanguageOpen}
+          >
+            <img src="https://www.ediya.com/images/common/lang_ko.webp" alt="한국어" />
             KO
           </a>
 
           <ul className="language-list">
-            <li>
-              <a href="">
-                <img
-                  src="https://www.ediya.com/images/common/lang_ko.webp"
-                  alt="한글"
-                />
-                KO
-              </a>
-            </li>
-
-            <li>
-              <a href="">
-                <img
-                  src="https://www.ediya.com/images/common/lang_en.webp"
-                  alt="영어"
-                />
-                EN
-              </a>
-            </li>
-
-            <li>
-              <a href="">
-                <img
-                  src="https://www.ediya.com/images/common/lang_cn.webp"
-                  alt="중국어"
-                />
-                CH
-              </a>
-            </li>
+            <li><a href="#"><img src="https://www.ediya.com/images/common/lang_ko.webp" alt="한국어" />KO</a></li>
+            <li><a href="#"><img src="https://www.ediya.com/images/common/lang_en.webp" alt="영어" />EN</a></li>
+            <li><a href="#"><img src="https://www.ediya.com/images/common/lang_cn.webp" alt="중국어" />CH</a></li>
           </ul>
         </div>
       </div>
 
-      {/* <!-- //로고 + 언어 --> */}
-
-      {/* <!-- 3구역: 메인 메뉴 (sub 펼쳐진 상태 고정) --> */}
-      <div className="menu-header">
+      {/* 3구역: 메인 메뉴 */}
+      {/* 🟨 [수정] 모바일 state에 따라 open 클래스 적용 */}
+      <nav
+        className={`menu-header ${isMobileMenuOpen ? "open" : ""}`}
+        aria-label="메인 메뉴"
+      >
         <div className="menu">
           <div className="menu-category">
             <a href="#">원두</a>
-
             <ul className="sub">
-              <li>
-                <a href="#">이디야 블렌드</a>
-              </li>
-              <li>
-                <a href="#">품질</a>
-              </li>
+              <li><a href="#">이디야 블렌드</a></li>
+              <li><a href="#">품질</a></li>
             </ul>
           </div>
 
           <div className="menu-category">
             <a href="#">메뉴</a>
-
             <ul className="sub">
-              <li>
-                <a href="#">음료</a>
-              </li>
-              <li>
-                <a href="#">푸드</a>
-              </li>
-              <li>
-                <a href="#">MD</a>
-              </li>
+              <li><a href="#">음료</a></li>
+              <li><a href="#">푸드</a></li>
+              <li><a href="#">MD</a></li>
             </ul>
           </div>
 
           <div className="menu-category">
             <a href="#">유통제품</a>
-
             <ul className="sub">
-              <li>
-                <a href="#">스틱커피</a>
-              </li>
-              <li>
-                <a href="#">커피믹스</a>
-              </li>
-              <li>
-                <a href="#">원두커피</a>
-              </li>
-              <li>
-                <a href="#">RTD 커피</a>
-              </li>
-              <li>
-                <a href="#">스틱 음료</a>
-              </li>
-              <li>
-                <a href="#">선물세트</a>
-              </li>
-              <li>
-                <a href="#">수출입거래</a>
-              </li>
-              <li>
-                <a href="#">B2B/특판/대리점 거래</a>
-              </li>
+              <li><a href="#">스틱커피</a></li>
+              <li><a href="#">커피믹스</a></li>
+              <li><a href="#">원두커피</a></li>
+              <li><a href="#">RTD 커피</a></li>
+              <li><a href="#">스틱 음료</a></li>
+              <li><a href="#">선물세트</a></li>
+              <li><a href="#">수출입거래</a></li>
+              <li><a href="#">B2B/특판/대리점 거래</a></li>
             </ul>
           </div>
 
           <div className="menu-category">
             <a href="#">이디야멤버스</a>
-
             <ul className="sub">
-              <li>
-                <a href="#">멤버십안내</a>
-              </li>
-              <li>
-                <a href="#">서비스안내</a>
-              </li>
+              <li><a href="#">멤버십안내</a></li>
+              <li><a href="#">서비스안내</a></li>
             </ul>
           </div>
 
           <div className="menu-category">
             <a href="#">상품권·제휴카드</a>
-
             <ul className="sub">
-              <li>
-                <a href="#">상품권 소개</a>
-              </li>
-              <li>
-                <a href="#">제휴카드(하나·신한)</a>
-              </li>
+              <li><a href="#">상품권 소개</a></li>
+              <li><a href="#">제휴카드(하나·신한)</a></li>
             </ul>
           </div>
 
           <div className="menu-category">
             <a href="#">브랜드 소식</a>
-
             <ul className="sub">
-              <li>
-                <a href="#">공지사항</a>
-              </li>
-              <li>
-                <a href="#">이벤트 안내</a>
-              </li>
-              <li>
-                <a href="#">사회공헌 활동</a>
-              </li>
+              <li><a href="#">공지사항</a></li>
+              <li><a href="#">이벤트 안내</a></li>
+              <li><a href="#">사회공헌 활동</a></li>
             </ul>
           </div>
 
           <div className="menu-category">
             <a href="#">고객 지원·케이터링</a>
-
             <ul className="sub">
-              <li>
-                <a href="#">고객의 소리</a>
-              </li>
-              <li>
-                <a href="#">소비자중심경영(CCM)</a>
-              </li>
-              <li>
-                <a href="#">제휴·제안</a>
-              </li>
-              <li>
-                <a href="#">케이터링·커피트럭</a>
-              </li>
+              <li><a href="#">고객의 소리</a></li>
+              <li><a href="#">소비자중심경영(CCM)</a></li>
+              <li><a href="#">제휴·제안</a></li>
+              <li><a href="#">케이터링·커피트럭</a></li>
             </ul>
           </div>
         </div>
-      </div>
-
-      {/* <!-- //메인 메뉴 --> */}
+      </nav>
     </header>
   );
 }
